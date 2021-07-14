f0ef25f8ab51421e4bf37fab90e1842024f04fca
Diptendu28 <82577969+Diptendu28@users.noreply.github.com>
2021-07-14 23:09:02 +0530

    name

---
+++ k.c
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//int main()
+//{
+////   printf("\\"  "\"Diptendu Karmakar\"\"  ");
+//  printf("\%cdiptendu karmakar\%c%c",'"','"','"');
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main() 
+//{
+//	printf("%c%c%s%c%c",'\\','"',"your_name\\",'"','"');
+//	return 0;
+//}
+//#include <stdio.h>
+//int main() 
+//{
+//	printf("%c%c",'%','d');
+//}
+#include <stdio.h>
+int main() 
+{
+	printf("%c%s%c",'"',"your_name",'"');
+	return 0;
+}
